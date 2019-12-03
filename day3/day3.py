9862c3141f1c9a14b94f818922c438f23eb9805c
Kobe Mertens <jacobignaas.mertens@ugent.be>
2019-12-03 16:17:31 +0100

    day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,11 +1,92 @@
+import math
+
+class Coord:
+    def __init__(self, coord, dist):
+        super().__init__()
+        self.coord = coord
+        self.dist = dist
+
+    def __eq__(self, o):
+        return self.coord == o.coord
+
+    def __hash__(self):
+        return hash(self.coord)
+
+    def __repr__(self):
+        return f"Coord({self.coord}, {self.dist})"
+
+
 def read_file_lines():
     with open('input.txt', 'r') as f:
         lines = f.readlines()
 
     return lines
 
+
+def add_vector(v1, v2):
+    return (v1[0] + v2[0], v1[1] + v2[1])
+
+
+def scalar_mult(a, v):
+    return (v[0]*a, v[1]*a)
+
+
+def parse_string(s):
+    coords = set()
+    loc = (0, 0)
+    dirs = {'R': (0,1), 'D': (1, 0), 'L': (0, -1), 'U': (-1, 0)}
+    dist = 1
+    for token in s.strip().split(','):
+        direction = dirs[token[0]]
+        amount = int(token[1:])
+        for i in range(1,amount+1):
+            dest = add_vector(loc, scalar_mult(i, direction))
+            coords.add(Coord(dest, dist))
+            dist += 1
+        loc = dest
+
+    return coords
+
+
+def part1(s1, s2):
+    coords1 = parse_string(s1)
+    coords2 = parse_string(s2)
+
+    intersections = coords1.intersection(coords2)
+
+    return min(abs(coord.coord[0]) + abs(coord.coord[1]) for coord in intersections)
+
+def part2(s1, s2):
+    coords1 = parse_string(s1)
+    coords2 = parse_string(s2)
+
+    intersections = coords1.intersection(coords2)
+
+    mini = math.inf
+
+    for intersection in intersections:
+        for coord in coords1:
+            if coord.coord == intersection.coord:
+                dist1 = coord.dist
+                break
+
+        for coord in coords2:
+            if coord.coord == intersection.coord:
+                dist2 = coord.dist
+                break
+
+        if dist1 + dist2 < mini:
+            mini = dist1 + dist2
+
+    return mini
+
+
 def main():
     lines = read_file_lines()
+    # dist = part1(lines[0], lines[1])
+    # print(dist)
+
+    print(part2(lines[0], lines[1]))
 
 if __name__ == '__main__':
     main()
--- day3/tests.py
+++ day3/tests.py
@@ -3,8 +3,39 @@ import unittest
 
 
 class Tests(unittest.TestCase):
-    def test(self):
-        self.assertEqual(1, 1)
+    def test1(self):
+        self.assertEqual(
+            part1(
+                "R8,U5,L5,D3",
+                "U7,R6,D4,L4"
+                ), 6
+        )
+        self.assertEqual(
+            part1(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83"
+                ), 159
+        )
+        self.assertEqual(
+            part1(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+                ), 135
+        )
+
+    def test2(self):
+        self.assertEqual(
+            part2(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83"
+                ), 610
+        )
+        self.assertEqual(
+            part2(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+                ), 410
+        )
 
 if __name__ == '__main__':
     unittest.main()
