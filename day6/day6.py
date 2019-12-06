8531e511cb8fea72126215dd9f472d3d7b1abc9e
Kobe Mertens <jacobignaas.mertens@ugent.be>
2019-12-06 15:42:29 +0100

    day6

--- day6/day6.py
+++ day6/day6.py
@@ -1,11 +1,76 @@
+class Node:
+    def __init__(self, parent=None):
+        super().__init__()
+        self.parent = parent
+
 def read_file_lines():
     with open('input.txt', 'r') as f:
         lines = f.readlines()
 
     return lines
 
+
+def part1(lines):
+    node_dict = {}
+    for line in lines:
+        parent, child = line.strip().split(')')
+        if parent not in node_dict:
+            parent_node = Node()
+            node_dict[parent] = parent_node
+        else:
+            parent_node = node_dict[parent]
+        if child not in node_dict:
+            node_dict[child] = Node(parent_node)
+        else:
+            node_dict[child].parent = parent_node
+
+    count = 0
+    for node in node_dict.values():
+        while node.parent is not None:
+            node = node.parent
+            count += 1
+
+    return count
+
+
+def part2(lines):
+    node_dict = {}
+    for line in lines:
+        parent, child = line.strip().split(')')
+        if parent not in node_dict:
+            parent_node = Node()
+            node_dict[parent] = parent_node
+        else:
+            parent_node = node_dict[parent]
+        if child not in node_dict:
+            node_dict[child] = Node(parent_node)
+        else:
+            node_dict[child].parent = parent_node
+
+    path = set()
+    node = node_dict['YOU']
+    while node.parent is not None:
+        path.add(node)
+        node = node.parent
+
+    san_count = 0
+    total = 0
+    node = node_dict['SAN']
+    while node not in path:
+        node = node.parent
+        total += 1
+
+    while node is not None:
+        node = node.parent
+        san_count += 1
+        total += 1
+
+    return len(path) + total - 2*san_count - 1
+
+
 def main():
     lines = read_file_lines()
+    print(part2(lines))
 
 if __name__ == '__main__':
     main()
--- day6/tests.py
+++ day6/tests.py
@@ -4,7 +4,38 @@ import unittest
 
 class Tests(unittest.TestCase):
     def test(self):
-        self.assertEqual(1, 1)
+        count = part1([
+            "COM)B",
+            "B)C",
+            "C)D",
+            "D)E",
+            "E)F",
+            "B)G",
+            "G)H",
+            "D)I",
+            "E)J",
+            "J)K",
+            "K)L",
+        ])
+        self.assertEqual(count, 42)
+
+    def test2(self):
+        count = part2([
+            "COM)B",
+            "B)C",
+            "C)D",
+            "D)E",
+            "E)F",
+            "B)G",
+            "G)H",
+            "D)I",
+            "E)J",
+            "J)K",
+            "K)L",
+            "K)YOU",
+            "I)SAN",
+        ])
+        self.assertEqual(count, 4)
 
 if __name__ == '__main__':
     unittest.main()
