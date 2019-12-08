8bdc0a67415340d56efd8afa50cea1b69303af0e
Kobe Mertens <jacobignaas.mertens@ugent.be>
2019-12-08 12:18:21 +0100

    solution day8

--- day8/day8.py
+++ day8/day8.py
@@ -1,11 +1,46 @@
+import math
+
 def read_file_lines():
     with open('input.txt', 'r') as f:
         lines = f.readlines()
 
     return lines
 
+
+def part1(image, width, height):
+    mini = [math.inf]
+    size = width * height
+    layers = [image[i:i+size] for i in range(0, len(image), size)]
+    for layer in layers:
+        counts = [0, 0, 0]
+        for number in layer:
+            if number in ['0', '1', '2']:
+                counts[int(number)] += 1
+        if counts[0] < mini[0]:
+            mini = counts
+
+    return mini[1] * mini[2]
+
+
+def part2(image, width, height):
+    size = width * height
+    layers = [list(image[i:i+size]) for i in range(0, len(image), size)]
+    result = layers[0]
+    for layer in layers:
+        for i in range(size):
+            layer_pix = layer[i]
+            result_pix = result[i]
+            if result_pix == '2':
+                result[i] = layer_pix
+
+    parts = [''.join(['█' if pixel == '1' else ' ' for pixel in result[i:i+width]]) for i in range(0, len(result), width)]
+
+    return '\n'.join(parts)
+
+
 def main():
     lines = read_file_lines()
+    print(part2(lines[0].strip(), 25, 6))
 
 if __name__ == '__main__':
     main()
--- day8/tests.py
+++ day8/tests.py
@@ -4,7 +4,12 @@ import unittest
 
 class Tests(unittest.TestCase):
     def test(self):
-        self.assertEqual(1, 1)
+        result = part1('123456789012', 3, 2)
+        self.assertEqual(result, 1)
+
+    def test2(self):
+        result = part2('0222112222120000', 2, 2)
+        self.assertEqual(result, '01\n10')
 
 if __name__ == '__main__':
     unittest.main()
