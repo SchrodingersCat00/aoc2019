7fa78d830b781e1aad158e65676258bc890b9855
Kobe Mertens <jacobignaas.mertens@ugent.be>
2019-12-10 13:53:57 +0100

    day9 part2

--- day9/day9.py
+++ day9/day9.py
@@ -1,11 +1,25 @@
+import computer
+
 def read_file_lines():
     with open('input.txt', 'r') as f:
         lines = f.readlines()
 
     return lines
 
+
+def part1(program):
+    sensor = computer.Program(program)
+    sensor.recv_input([2])
+    sensor.run()
+    if sensor.is_suspended:
+        raise ValueError("The machine was suspended")
+
+    return sensor.send_output()
+
 def main():
     lines = read_file_lines()
+    program = [int(code) for code in lines[0].strip().split(',')]
+    print(part1(program))
 
 if __name__ == '__main__':
     main()
--- day9/tests.py
+++ day9/tests.py
@@ -1,10 +1,20 @@
 from day9 import *
 import unittest
+import computer
 
 
 class Tests(unittest.TestCase):
     def test(self):
-        self.assertEqual(1, 1)
+        result = part1([109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
+        self.assertEqual(result, [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
+
+    def test2(self):
+        [result] = part1([1102,34915192,34915192,7,4,7,99,0])
+        self.assertEqual(len(str(result)), 16)
+
+    def test3(self):
+        [result] = part1([104,1125899906842624,99])
+        self.assertEqual(result, 1125899906842624)
 
 if __name__ == '__main__':
     unittest.main()
